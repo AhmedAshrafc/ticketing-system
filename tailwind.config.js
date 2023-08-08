bd5529ff57565e059b5d175cb8eea26746e8acfc
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2023-08-08 17:21:20 +0300

    Change fluid property to new grid value

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         "main-bg-color": "#b5c2ca",
       },
       gridTemplateColumns: {
-        fluid: "repeat(auto-fit, minmax(15rem, 1fr))",
+        fluid: "repeat(auto-fill, minmax(250px, 1fr))",
       },
       fontFamily: {
         sans: ["Oswald", "sans-serif"],
