671bdc3b465bf41de2904fceabfebe256a1970a4
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2023-08-11 14:52:34 +0300

    Add ScrollTopButton component as a part of main app layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,16 @@
 import Header from "./Header";
+import ScrollTopButton from "./ScrollTopButton";
 import Sidebar from "./Sidebar";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-screen bg-gradient-to-b from-[#141E30] to-[#243B55] scrollbar">
+    <div className="flex min-h-screen bg-gradient-to-b from-[#141E30] to-[#243B55] scrollbar ">
       <Sidebar />
+      <ScrollTopButton />
       <div className="flex flex-col w-full">
         <Header />
+        {/* create a main content that doesn't make the sidebar and header components move if there is a scroll */}
+        {/* <main className="flex-grow p-4 overflow-y-auto scrollbar">{children}</main> */}
         <main className="bg-[#f1f5f9] p-4 shadow-2xl h-full">{children}</main>
       </div>
     </div>
