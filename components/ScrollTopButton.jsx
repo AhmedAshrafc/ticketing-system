cba90db8082d7b06c0881c2f566926e83f7cb8d8
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2023-08-11 14:52:04 +0300

    Add scroll to top button feature

---
+++ components/ScrollTopButton.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function ScrollTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+  const prevScrollPos = useRef(0);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      const currentScrollPos = window.scrollY;
+
+      if (currentScrollPos > 500 && currentScrollPos > prevScrollPos.current) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+
+      prevScrollPos.current = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, [isVisible]);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          className="fixed bottom-4 right-4 bg-[#243B55] text-white p-2 rounded-full shadow-2xl transition duration-500 ease-in-out transform hover:scale-110 hover:bg-[#141E30]"
+          onClick={() => {
+            scrollToTop();
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white animate-bounce"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5 11l7-7 7 7M5 19l7-7 7 7"
+            />
+          </svg>
+        </button>
+      )}
+    </>
+  );
+}
