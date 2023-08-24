ef82db2e9c61dd9ebb2d1053287bb1bea5d6cc25
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2023-08-24 16:52:42 +0300

    Redesign dark mode from a button to a toggle

--- components/DarkModeToggle.jsx
+++ components/DarkModeToggle.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+
 import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
+
 import { Tooltip } from "react-tooltip";
 
 export default function DarkModeToggle({ isOpen }) {
@@ -33,32 +35,48 @@ export default function DarkModeToggle({ isOpen }) {
       data-tooltip-id="darkmode-tooltip"
       data-tooltip-place="right"
     >
-      {!isOpen && (
-        <Tooltip
-          id="darkmode-tooltip"
-          content="Dark Mode"
-          style={{ zIndex: "9999" }}
-        />
-      )}
-      <button
-        onClick={toggleTheme}
-        className="flex items-center w-full text-md text-black dark:text-white rounded-md p-2 transition-all group-hover:bg-[#081225] dark:hover:bg-secondary-color group-hover:text-white group-active:bg-[#081225]"
-      >
-        <div className="flex items-center gap-3">
-          {theme === "light" ? (
-            <HiOutlineMoon className=" min-w-[20px] text-black dark:text-white group-hover:text-white transition-all ml-2" />
-          ) : (
-            <HiOutlineSun className=" min-w-[20px] text-black dark:text-white group-hover:text-white transition-all ml-2" />
-          )}
-          <span
-            className={`hidden md:inline-block duration-300 font-semibold ${
-              !isOpen && "scale-0"
-            }`}
-          >
-            {theme === "light" ? "Dark Mode" : "Light Mode"}
-          </span>
-        </div>
-      </button>
+      <div>
+        {!isOpen && (
+          <Tooltip
+            id="darkmode-tooltip"
+            content="Dark Mode"
+            style={{ zIndex: "9999" }}
+            offset={-50}
+          />
+        )}
+        <label className="flex items-center p-2 cursor-pointer">
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+            className="hidden w-full"
+          />
+          <div className="flex items-center gap-4">
+            <div
+              className={`relative w-10 h-5 rounded-full bg-gray-300 dark:bg-gray-600`}
+            >
+              <div
+                className={`absolute flex items-center justify-center top-0 left-0 w-5 h-5 rounded-full bg-white dark:bg-black transform transition-transform duration-300 ease-in-out ${
+                  theme === "dark" ? "translate-x-5" : ""
+                }`}
+              >
+                {theme === "dark" ? (
+                  <HiOutlineMoon className="w-4 h-4 text-black dark:text-white" />
+                ) : (
+                  <HiOutlineSun className="w-4 h-4 text-black dark:text-white" />
+                )}
+              </div>
+            </div>
+            <span
+              className={`hidden md:inline-block duration-300 dark:text-white font-semibold ${
+                !isOpen && "scale-0"
+              }`}
+            >
+              {theme === "dark" ? "Dark Mode" : "Light Mode"}
+            </span>
+          </div>
+        </label>
+      </div>
     </li>
   );
 }
