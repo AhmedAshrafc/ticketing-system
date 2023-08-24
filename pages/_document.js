3a33a317e2f3f4260e5c44a5ca4d6db25c8b3b13
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2023-08-24 16:55:36 +0300

    Add meta tags for better optimizations

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="A.U.H. Dashboard" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="description" content="Dashboard for Hospital Website" />
+        <meta name="keywords" content="hospital, dashboard, website" />
+
+        <link rel="icon" href="/favicon.png" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
